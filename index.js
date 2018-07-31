adef97582b9c03cc8d4aa2bec12dbd5dbee01781
sam ditson <samditson2@hotmail.co.uk>
2018-07-31 17:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function happyHolidays(string){
-  return `Happy holidays!`}
+  return `Happy holidays!`
+  
+}
 
 function happyHolidaysTo(string){
-  return `Happy holidays, ${string}!`}
+  return `Happy holidays, ${string}!`
+  
+}
   
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days, holiday){
+  return `It's ${days} days until ${holiday}!`
 }
